be28a8f1a17e09eb9d2b0e0789c3ae8b99e5c1b1
Hojjat <hojjat12000@yahoo.com>
2022-04-06 22:53:12 -0600

    made some changes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use gtk::gdk_pixbuf::Pixbuf;
 use gtk::prelude::*;
 use gtk::Application;
 use gtk::ApplicationWindow;
 
 use std::fs;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
+
+//TODO(Hojjat): Need to update the CellRenderers: https://python-gtk-3-tutorial.readthedocs.io/en/latest/cellrenderers.html
 
 fn activate(app: &Application) {
     let windows = ApplicationWindow::builder()
@@ -16,16 +19,36 @@ fn activate(app: &Application) {
 
     let sw = gtk::ScrolledWindow::builder().build();
 
-    let icon = gtk::gdk_pixbuf::Pixbuf::from_file("a.jpg").expect("couldn't open a.jpg");
-    let name = String::from("Hello");
+    // let icon = gtk::gdk_pixbuf::Pixbuf::from_file("a.jpg").expect("couldn't open a.jpg");
 
+    let icon = gtk::IconTheme::default()
+        .unwrap()
+        .load_icon("edit-cut", 64, gtk::IconLookupFlags::GENERIC_FALLBACK)
+        .expect("Couldn't find the icon")
+        .unwrap();
+
+    let (tx, rx): (Sender<f64>, Receiver<f64>) = mpsc::channel();
+
+    let scale = gtk::Scale::builder()
+        .orientation(gtk::Orientation::Horizontal)
+        .digits(1)
+        .show_fill_level(true)
+        .round_digits(1)
+        .upper_stepper_sensitivity(gtk::SensitivityType::Auto)
+        .lower_stepper_sensitivity(gtk::SensitivityType::Auto)
+        .build();
+
+    scale.set_range(0.0, 1.0);
+    let scale_tx = tx.clone();
+    scale.connect_value_changed(move |v| {
+        println!("{:?}", v.value());
+        scale_tx.send(v.value()).unwrap();
+    });
     println!("{:?}", icon.type_());
+
     let store = gtk::ListStore::new(&[gio::glib::Type::STRING, icon.type_()]);
 
     let paths = fs::read_dir("./").unwrap();
-
-    let mut names: Vec<(u32, &dyn ToValue)> = vec![];
-    let mut icons: Vec<(u32, &dyn ToValue)> = vec![];
 
     let mut names_str = vec![];
 
@@ -39,21 +62,26 @@ fn activate(app: &Application) {
         store.set(&iter, &[(1, &icon)]);
     }
 
-    // store.set(&iter, names.as_slice());
-    // store.set(&iter, icons.as_slice());
-    // let iter = store.append();
-    // store.set(&iter, &[(0, &"hello")]);
-    // store.set(&iter, &[(1, &icon)]);
-    // let iter = store.append();
-    // store.set(&iter, &[(0, &"hello")]);
-    // store.set(&iter, &[(1, &icon)]);
-    let icon_view = gtk::IconView::builder().model(&store).build();
+    let a: f64 = rx.try_recv().unwrap_or(1.0);
+
+    let icon_view = gtk::IconView::builder()
+        .model(&store)
+        .item_width(((100 as f64) * a).round() as i32)
+        .item_padding(5)
+        .build();
 
     icon_view.set_text_column(0);
     icon_view.set_pixbuf_column(1);
 
     sw.add(&icon_view);
-    windows.add(&sw);
+    let gtkbox = gtk::Box::builder()
+        .spacing(6)
+        .orientation(gtk::Orientation::Vertical)
+        .build();
+    gtkbox.add(&sw);
+    // gtkbox.add(&scale);
+    // windows.add(&gtkbox);
+    windows.add(&scale);
     windows.show_all();
 }
 
